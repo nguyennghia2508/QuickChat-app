0108c0c9277933e45582212bacf19e148b7889c3
Nguyen Thanh Nghia <binlatao3@gmail.com>
2024-07-30 23:47:23 +0700

    v4.0

--- src/app/actions/getSession.ts
+++ src/app/actions/getSession.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../libs/configs/auth/authOptions";
 
 export default async function getSession() {
   return await getServerSession(authOptions);
